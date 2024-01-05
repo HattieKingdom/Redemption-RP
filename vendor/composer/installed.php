7564ececc85d601fde5ceb244d410466484b3a97
HattieKingdom <85170201+HattieKingdom@users.noreply.github.com>
2024-01-05 19:08:26 +0800

    Composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'francoisjacquet/rosariosis',
+        'pretty_version' => 'dev-mobile',
+        'version' => 'dev-mobile',
+        'reference' => '25801cfdb61dd1524f031fae081fca2ca7dfc5b0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'francoisjacquet/rosariosis' => array(
+            'pretty_version' => 'dev-mobile',
+            'version' => 'dev-mobile',
+            'reference' => '25801cfdb61dd1524f031fae081fca2ca7dfc5b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
